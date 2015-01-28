a92b01f046eacce19898beef347d5532dfb58254
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-01-28 17:37:33 +0100

    get sections

--- TP1/src/AutoDoc.java
+++ TP1/src/AutoDoc.java
@@ -1,23 +1,32 @@
 import java.awt.List;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Stack;
+
 /**
- * la classe AutoDoc concrétise la classe abstraite Automate
+ * la classe AutoDoc concretise la classe abstraite Automate
  * pour l'analyse d'un document reconnaissable par un automate
  * fini
- * @author : Marie-Jo Pédrono
- * date de création : 18 mars 2003
+ * @author : Marie-Jo Pï¿½drono
+ * date de crï¿½ation : 18 mars 2003
  */
-public class AutoDoc extends Automate {
+public class AutoDoc extends Automate
+{
+	// resulting-file name
+	String fileName = "result.html";
+	// error state
+	int e = etatErreur;
+	// contains keywords
 	ArrayList<String> keyWords = new ArrayList<String>();
-	boolean etoilelue = false;
-	int compteur = 1;
-    int e = etatErreur;// à des fins de présentation de la matrice
+	// levels-managing of the document (we suppose the document does not have more than 10 imbricated paragraphs)
+	int[] levels = new int[10];
+	int levelIndex = 0;
+	// true when you've just read an asterisk (so keyword is following), false otherwise
+	boolean asteriskDetected = false;
 
-    //la matrice de transition n° ligne : état, n° colonne : unite
-    
-
-  private  final int[][] transition = {
+    //la matrice de transition
+	private  final int[][] transition = {
   //autre,doc,fdoc,tit,ftit,date,fdate,para,fpara,ident,entier,pt,virg,etoi  //etat
     {e,    1,  e,   e,   e,   e,   e,    e,   e,    e,   e,     e,  e ,e },//0
     {e,    e,  e,   e,   e,   2,   e,    e,   e,    e,   e,     e,  e ,e },//1
@@ -33,22 +42,23 @@ public class AutoDoc extends Automate {
 
 /**
 * Constructeur AutoDoc pour initialisation
-* initialise l’état initial et l’état final 
-* @param le flot d'entrée
+* initialise lï¿½ï¿½tat initial et lï¿½ï¿½tat final 
+* @param flot : le flot d'entrï¿½e
 * 
 */
     public AutoDoc(InputStream flot)
     { 
-    	    super(flot);
-    	    this.etatInitial = 0;
-    	    this.etatFinal = 10;
+    	super(flot);
+    	this.etatInitial = 0;
+    	this.etatFinal = 10;
+		Ecriture.clearFile(fileName);
     }
 
   /**
-   *  Methode qui calcule l'état suivant
-   *  @param etat : l'état courant
-   *  @param unite : l'unité du token courant
-   *  @return l'état suivant
+   *  Methode qui calcule l'ï¿½tat suivant
+   *  @param etat : l'ï¿½tat courant
+   *  @param unite : l'unitï¿½ du token courant
+   *  @return l'ï¿½tat suivant
    */
   int getTransition(int etat, int unite)
   {
@@ -58,93 +68,94 @@ public class AutoDoc extends Automate {
   
   void faireAction(int etat, int unite)
   {
-	  Ecriture.ouvrir("resultat.txt");
+	  String file = this.fileName;
 	  switch (unite)
 	  {
-	  case Constants.AUTRE:
-		  // do nothing
-		  break;
-		  
-	  case Constants.DATE:
-		  Ecriture.ecrireString("\n<center>\n<h1>");
-		  break;
-		  
-	  case Constants.DOC:
-		  Ecriture.ecrireString("<html>\n<body>\n");
-		  break;
-		  
-	  case Constants.ENTIER:
-		  Ecriture.ecrireString(this.token.lexeme);
-		  break;
-		  
-	  case Constants.EOF:
-		  
-		  break;
-	  		
-	  case Constants.ETOILE:
-		  this.etoilelue = true;
-		  break;
-		  
-	  case Constants.FINDATE:
-		  Ecriture.ecrireString("<\\h1><\\center>");
-		  break;
-		  
-	  case Constants.FINDOC:
-		  
-		  break;
-		  
-	  case Constants.FINPARA:
-		  
-		  break;
-	  
-	  case Constants.FINTITRE:
-		  Ecriture.ecrireString("<\\h1><\\center>\n");
-		  break;
+		  case Constants.AUTRE:
+			  // do nothing
+			  break;
 
-	  case Constants.IDENT:
-		  Ecriture.ecrireString(this.token.lexeme);
-		  if (this.etoilelue) {this.keyWords.add(this.token.lexeme);this.etoilelue=false;}
-		  break;
-		  
-	  case Constants.PARA:
-		  /*
-		  int level = this.compteur;
-		  if (level == 1)
-		  {
-			  Ecriture.ecrireString("<h"+this.compteur+">\n"+this.compteur);
-		  }
-		  
-		  
-		  
-		  if (compteur > 1) 
-		  {
-			  Ecriture.ecrireString("<\\h"+(this.level-1)+">\n");
-			  Ecriture.ecrireString("<h"+this.level+">");
-			  while(level>1)
+		  case Constants.DATE:
+			  Ecriture.ecrireString(file,"\n<center>\n<h1>");
+			  break;
+
+		  case Constants.DOC:
+			  Ecriture.ecrireString(file,"<html>\n<body>\n");
+			  break;
+
+		  case Constants.ENTIER:
+			  Ecriture.ecrireString(file,this.token.lexeme);
+			  break;
+
+		  case Constants.EOF:
+
+			  break;
+
+		  case Constants.ETOILE:
+			  this.asteriskDetected = true;
+			  break;
+
+		  case Constants.FINDATE:
+			  Ecriture.ecrireString(file,"<\\h1><\\center>");
+			  break;
+
+		  case Constants.FINDOC:
+
+			  break;
+
+
+
+		  case Constants.FINTITRE:
+			  Ecriture.ecrireString(file,"<\\h1><\\center>\n");
+			  break;
+
+		  case Constants.IDENT:
+			  Ecriture.ecrireString(file,this.token.lexeme);
+			  if (this.asteriskDetected)
 			  {
-				  Ecriture.ecrireString("&nbsp;&nbsp;&nbsp;");
-				  level--;
+				  // add keyword to the list of keywords
+				  this.keyWords.add(this.token.lexeme);
+				  this.asteriskDetected=false;
 			  }
-		  }
-		  
-		  Ecriture.ecrireString("<h"+this.compteur+"> \n"+this.compteur);
-		  this.compteur++;
-		  */
-		  break;
-		  
-	  case Constants.POINT:
-		  
-		  break;
-		  
-	  case Constants.TITRE:
-		  Ecriture.ecrireString("<center><h1>\n");
-		  break;
-		  
-	  case Constants.VIRGULE:
-		  
-		  break;
-		  
-		  
+			  break;
+
+		  case Constants.PARA:
+			  levels[++levelIndex]++;
+			  Ecriture.ecrireString(file,"<h"+levelIndex+1+">");
+
+			  String numerotation = "";
+			  int lvl=1;
+			  numerotation += " "+levels[lvl]+".";
+			  while (lvl<levelIndex)
+			  {
+				  numerotation = "&nbsp;&nbsp;&nbsp;" + numerotation;
+				  numerotation += levels[lvl] + ".";
+				  lvl++;
+			  }
+			  Ecriture.ecrireString(file,numerotation);
+			  break;
+
+		  case Constants.FINPARA:
+			  Ecriture.ecrireString(file,"</h"+levelIndex+">");
+			  for (int i=levelIndex+1;i<10;i++)
+			  {
+				  levels[i]=0;
+			  }
+			  levelIndex--;
+			  break;
+
+		  case Constants.POINT:
+
+			  break;
+
+		  case Constants.TITRE:
+			  Ecriture.ecrireString(file,"<center><h1>\n");
+			  break;
+
+		  case Constants.VIRGULE:
+
+			  break;
 	  }
+
   };
 }
