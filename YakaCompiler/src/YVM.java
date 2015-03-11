ebfd179fa38b37fe97dc55cbbd42b93dde163dcf
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-03-11 11:15:00 +0100

    YVM done

--- YakaCompiler/src/YVM.java
+++ YakaCompiler/src/YVM.java
@@ -1,9 +1,66 @@
 
 public class YVM 
 {
+	protected int allocatedMemory = 0;
+	
+	public void alloc()
+	{
+		this.allocatedMemory += 2;
+	}
+	
+	
 	public void entete()
 	{
-		//Ecriture.ecrire(Yaka.filename,"entete");
+		Ecriture.ecrireString(Yaka.YVMfilename, "entete\n");
+	}
+	
+	public void queue()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "queue\n");
+	}
+	
+	public void ouvrePrinc()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "ouvrePrinc "+this.allocatedMemory+"\n");
+	}
+	
+	public void iconst(int value)
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "iconst "+value+"\n");
+	}
+	
+	public void istore(int value)
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "istore "+value+"\n");
 	}
 
+	public void iadd ()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "iadd\n");
+	}
+	
+	public void isub()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "isub\n");
+	}
+	
+	public void imul()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "imul\n");
+	}
+	
+	public void idiv()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "idiv\n");
+	}
+	
+	public void iinfegal()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "iinfegal\n");
+	}
+	
+	public void ior()
+	{
+		Ecriture.ecrireString(Yaka.YVMfilename, "ior\n");
+	}
 }
--- YakaCompiler/src/YakaToAsm.java
+++ YakaCompiler/src/YakaToAsm.java
@@ -1,8 +1,117 @@
 
 public class YakaToAsm extends YVM
 {
+	
+	private void write (String s){
+		Ecriture.ecrireString(Yaka.ASMfilename, "\t"+s);
+	}
+	
+	public void test()
+	{
+		this.entete();
+		this.ouvrePrinc();
+		this.iconst(10);
+		this.idiv();
+		this.iadd();
+		this.istore(566);
+		this.ior();
+		this.iinfegal();
+		this.queue();
+	}
+	
 	public void entete()
 	{
-		//Ecriture.ecrire(Yaka.filename,"; entete\n.model SMALL\n.586\n\n");
+		Ecriture.ecrireString(Yaka.ASMfilename, ";entete\n"
+				+ ".model SMALL\n"
+				+ ".586\n"
+				+ ".CODE\n"
+				+ "debut :\n"
+				+ "\tSTARTUPCODE\n");
+	}
+	
+	public void queue()
+	{
+		this.write(";queue\n"
+				+ "\tnop\n"
+				+ "\texitcode\n"
+				+ "\tend debut\n");
+	}
+	
+	public void ouvrePrinc()
+	{
+		this.write(";ouvrePrinc"+this.allocatedMemory+"\n"
+				+ "\tmov bp,sp\n"
+				+ "\tsub sp,"+this.allocatedMemory+"\n");
+	}
+	
+	public void iconst(int value)
+	{
+		this.write(";iconst "+value+"\n"
+				+ "\tpush "+value+"\n");
+	}
+	
+	public void istore(int value)
+	{
+		this.write(";istore "+value+"\n"
+				+ "\tpop ax\n"
+				+ "\tmov word ptr [bp-"+value+"],ax\n");
+	}
+
+	public void iadd ()
+	{
+		this.write(";iadd\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\tadd ax,bx\n"
+				+ "\tpush ax\n");
+	}
+	
+	public void isub()
+	{
+		this.write(";isub\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\tsub ax,bx\n"
+				+ "\tpush ax\n");
+	}
+	
+	public void imul()
+	{
+		this.write(";imul\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\timul bx\n"
+				+ "\tpush ax\n");
+	}
+	
+	public void idiv()
+	{
+		this.write(";idiv\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\tcwd\n"
+				+ "\tidiv bx\n"
+				+ "\tpush ax\n");
+	}
+	
+	public void iinfegal()
+	{
+		this.write(";iinfegal\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\tcmp ax,bx\n"
+				+ "\tjg $+6\n"
+				+ "\tpush -1\n"
+				+ "\tjmp $+4\n"
+				+ "\tpush 0\n");
+	}
+	
+	public void ior()
+	{
+		this.write(";ior\n"
+				+ "\tpop bx\n"
+				+ "\tpop ax\n"
+				+ "\tor ax,bx\n"
+				+ "\tpush ax\n");
 	}
 }
